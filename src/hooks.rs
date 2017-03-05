d61d9497bebe593c38f3b78c011e4891883c3c97
Alexander Weld <weldale@gmail.com>
2017-03-05 15:55:54 -0800

    pass arguments

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,16 +1,36 @@
 
 use ::ui::UiResult;
+use ::cmd::Command as Cmd;
 
 use std::path::{
     Path,
     PathBuf,
 };
 
-use std::process::Command;
+use std::process::{
+    Command,
+    Stdio,
+};
+
+use std::io::{
+    BufWriter,
+    Write,
+};
 
 #[derive(Debug)]
 pub struct Hooks {
     post_change: Option<PathBuf>
+}
+
+fn get_hook_str<'a>(command: &'a Cmd) -> (&'a str, Option<&'a str>, Option<&'a str>) {
+    match *command {
+        Cmd::Init => ("init", None, None),
+        Cmd::ListKeys => ("ls", None, None),
+        Cmd::PutString(ref key, ref val) => ("put", Some(key.as_str()), Some(val.as_str())),
+        Cmd::Drop(ref key) => ("drop", Some(key.as_str()), None),
+        Cmd::CreateEmptyList(ref key) => ("emptyList", Some(key.as_str()), None),
+        _ => unimplemented!()
+    }
 }
 
 impl Hooks {
@@ -41,16 +61,39 @@ impl Hooks {
         }
     }
 
-    pub fn run_post_change_hook(&self, result: &UiResult) -> Result<bool, String> {
+    pub fn run_post_change_hook(&self, result: &UiResult, command: &Cmd) -> Result<bool, String> {
         match self.post_change {
             Some(ref post_change_hook) => {
-                let status = try!(Command::new(post_change_hook).status().map_err(|e| e.to_string()));
+
+                let (action, key_opt, val_opt) = get_hook_str(command);
+
+                let mut cmd_builder = Command::new(post_change_hook);
+                cmd_builder.stdin(Stdio::piped());
+
+                cmd_builder.arg(action);
+                
+                if let Some(key) = key_opt {
+                    cmd_builder.arg(key);
+                }
+
+                if let Some(val) = val_opt {
+                    cmd_builder.arg(val);
+                }
+
+                let mut hook_child = try!(cmd_builder.spawn().map_err(|e| e.to_string()));
+                {
+                    let stdin = try!(hook_child.stdin.as_mut().ok_or("no stdin".to_string()));
+                    let mut stdin_buf = BufWriter::new(stdin);
+                    try!(write!(stdin_buf, "{}", result).map_err(|e| e.to_string()));
+                }
+                let status = try!(hook_child.wait().map_err(|e| e.to_string()));
                 Ok(status.success())
             },
 
             None => Ok(false),
         }
     }
+
 }
 
 
@@ -59,6 +102,7 @@ mod tests {
     use super::*;
 
     use ::ui::UiResult;
+    use ::cmd::Command;
 
     #[test]
     fn test_load_from_dir() {
@@ -71,7 +115,7 @@ mod tests {
     fn test_run_post_change() {
         let hooks = Hooks::load_from_dir("test/hooks");
 
-        let result = hooks.run_post_change_hook(&UiResult::Ok);
+        let result = hooks.run_post_change_hook(&UiResult::Ok, &Command::Init);
 
         assert_eq!(Ok(true), result);
     }
